b66265a3a1b204493196ea21ddf37a709fec3c1a
matheusdsanjos <matheusdsanjos@outlook.com>
2024-05-12 17:12:00 -0300

    Criação de toda a lógica para exibir o saldo de vitórias e o nível do herói

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function calcularRankeado(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+const resultado = calcularRankeado(70, 20);
+console.log(`O Herói tem saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
